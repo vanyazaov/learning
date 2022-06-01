fff88da1a73aa06e68bab3e78fdffb2ffeaff3da
Ivan <vankazaov@gmail.com>
2022-06-01 09:29:02 +0300

    Внешний вид и тесты

---
+++ tests/LocatorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Vankazaov\Ip2geoLocator;
+
+use http\Exception\InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class LocatorTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $locator = new Locator();
+        $location = $locator->locate('8.8.8.8');
+
+        self::assertNotNull($location);
+        self::assertEquals('United States', $location->getCountry());
+        self::assertEquals('California', $location->getRegion());
+        self::assertEquals('Mountain View', $location->getCity());
+    }
+
+    public function testNotFound(): void
+    {
+        $locator = new Locator();
+        $location = $locator->locate('127.0.0.1');
+
+        self::assertNull($location);
+    }
+
+    public function testInvalid(): void
+    {
+        $locator = new Locator();
+
+        $this->expectException(InvalidArgumentException::class);
+        $location = $locator->locate('invalid');
+    }
+
+    public function testEmpty(): void
+    {
+        $locator = new Locator();
+
+        $this->expectException(InvalidArgumentException::class);
+        $location = $locator->locate('');
+    }
+
+}
