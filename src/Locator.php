44e6e19a6120059f2e7f77eeef6cf193935be2a3
Ivan <vankazaov@gmail.com>
2022-06-02 08:36:34 +0300

    Запрос в API

--- src/Locator.php
+++ src/Locator.php
@@ -17,5 +17,12 @@ class Locator
         if (filter_var($ip, FILTER_VALIDATE_IP) === false) {
             throw new InvalidArgumentException('Invalid IP ' . $ip);
         }
+
+        $url = 'https://api.ipgeolocation.io/ipgeo?' . http_build_query([
+                'apiKey' => 'b72326c0a81b487f85804eb84f433b8a',
+                'ip' => $ip
+        ]);
+
+        $response = file_get_contents($url);
     }
 }
